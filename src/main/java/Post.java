49a07e3a863b13863fc640721fd32f96f530dfc2
Kirylbas <kirill.kolobov.98@gmail.com>
2023-03-21 12:55:57 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public boolean subscription;
+    public String phone;
+    public String passport;
+    public FormDate birthday;
+}
